2b11c8d0aa90a89cc7b09f7903abf9e5d2017f3a
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2024-07-19 16:04:52 +0100

    updated script

---
+++ script.js
@@ -0,0 +1,20 @@
+const numberInput = document.getElementById('number');
+const convertBtn = document.getElementById('convert-btn');
+const result = document.getElementById('output');
+
+
+const checkInput = (e) => {
+    if (!numberInput.value) {
+        result.innerText = "Please enter a valid number"
+        result.style.color = 'red' 
+    } else if (numberInput.value < 0) {
+        result.innerText = "Please enter a number greater than or equal to 1"
+        result.style.color = 'red'
+    } else if (numberInput.value >= 4000) {
+        result.innerText = "Please enter a number less than or equal to 3999"
+    }
+
+    e.preventDefault()
+}
+
+convertBtn.addEventListener('click', checkInput)
